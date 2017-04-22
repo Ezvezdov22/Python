5f25594db3546994e8831d0029ce96448768f2e0
Ezvezdov <ezvezdov22@gmail.com>
2017-04-22 13:21:15 +0300

    #

--- 358.py
+++ 358.py
@@ -1,23 +1,15 @@
 n, m = map(int, input().split())
-
-#index=0
-A=[]
-maximum=0
-allmax=0
-
-tmp = []
+maximum = 0
 
 for i in range(n):
-    A.append(list(map(int, input().split())))
-    best = max(A[i])
-    if maximum <= best:
+    lst = list(map(int, input().split()))
+    best = max(lst)
+    summa = sum(lst)
+    if maximum < best:
         maximum = best
-
-for i in range(n):
-    summa = sum(A[i])
-    if max(A[i]) == maximum and allmax < summa:
-            allmax = summa
-            index = i
+        allmax = 0
+    if maximum == best and allmax < summa:
+        allmax = summa
+        index = i
 
 print(index)
-
